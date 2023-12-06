66d9ab76fc464bff59a0f30c260a12a85a497a14
Wing Leung <wing.leung@vrt.be>
2023-12-06 15:59:48 +0100

    add scenario

---
+++ app/routes/some-article[~]works.tsx
@@ -0,0 +1,26 @@
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export function loader({ params }: LoaderFunctionArgs) {
+  const {path, pageId} = params
+
+  return json({
+    path,
+    pageId
+  })
+}
+
+export default function Index() {
+  const {
+    path,
+    pageId
+  } = useLoaderData<any>()
+
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      hello
+      {path}
+      {pageId}
+    </div>
+  );
+}
